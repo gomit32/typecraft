7113054bbcc9d54a48718f24ebbd61ea61c1859f
Gomit Tyagi <144540508+gomit32@users.noreply.github.com>
2026-02-25 18:52:13 +0530

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaHeart, FaKeyboard, FaGithub } from 'react-icons/fa';
+import { FaHeart } from 'react-icons/fa';
 import './Footer.css';
 
 const Footer = () => {
